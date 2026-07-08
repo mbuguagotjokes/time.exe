ef5fa8a91160b8a0f050eab61d586fd1e99c418d
mbuguagotjokes <calvinmbuguam@gmail.com>
2026-07-08 11:03:17 +0300

    Updated something small. PS: error output only gives oly gives out status code and i dont seem to care

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"time"
+	"fmt"
+	"io"
 	"os"
 	"os/exec"
-	"fmt"
+	"time"
 )
 
 func main() {
@@ -13,21 +14,40 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Please feed to the program another program to execute")
 		os.Exit(1)
 	}
+	
 	start := time.Now()
 	cmd := exec.Command(args[0], args[1:]...)
-	err := cmd.Run()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "\nError during execution: \n\t %v\n\n", err.Error())
+		fmt.Fprintf(os.Stderr, "\nStdoutPipe Err: \n\n\t %v\n\n", err)
+		os.Exit(1)
+	}
+	
+	err = cmd.Start()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\nError during execution: \n\n\t %v\n\n", err)
 		os.Exit(1)
 	}
 
-	output, err := cmd.Output()
+	output, err := io.ReadAll(stdout)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "\nstdout: \n\t %v\n\n", err.Error())
-	} else if len(output) > 0 {
-		fmt.Println(string(output))
+	    fmt.Fprintf(os.Stderr, "\nError reading stdout: \n\n\t%v\n\n", err)
+	    os.Exit(1)
+	}
+	
+	if err := cmd.Wait(); err != nil {
+	  	fmt.Fprintf(os.Stderr, "\nError during execution: \n\n\t %v\n\n", err)
+		os.Exit(1)
+	}
+
+	if len(output) > 0 {
+		fmt.Printf("\nStdout: \n\n\t %v\n\n", string(output))
+	} else {
+		fmt.Println("Run succesfuly")
 	}
 
 	
-	defer fmt.Println(time.Since(start).String())
+	defer func() {
+		fmt.Println(time.Since(start).String())
+	} ()
 }
